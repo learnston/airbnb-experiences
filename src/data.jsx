533320b3a09afdc82b5d9ba4de353e003792fcb6
learnston <luisf91mv@gmail.com>
2022-10-31 15:39:22 +0100

    adjust card image route in another way 2

--- src/data.jsx
+++ src/data.jsx
@@ -5,7 +5,8 @@ export default [
     description:
       'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
     price: 136,
-    coverImg: '/src/assets/katie.png',
+    coverImg:
+      'https://raw.githubusercontent.com/learnston/airbnb-experiences/master/src/assets/katie.png',
     stats: {
       rating: 5.0,
       reviewCount: 6,
@@ -19,7 +20,8 @@ export default [
     description:
       "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
     price: 125,
-    coverImg: '/src/assets/wedding.png',
+    coverImg:
+      'https://raw.githubusercontent.com/learnston/airbnb-experiences/master/src/assets/wedding.png',
     stats: {
       rating: 5.0,
       reviewCount: 30,
@@ -33,7 +35,8 @@ export default [
     description:
       'Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)',
     price: 50,
-    coverImg: '/src/assets/bike.png',
+    coverImg:
+      'https://raw.githubusercontent.com/learnston/airbnb-experiences/master/src/assets/bike.png',
     stats: {
       rating: 4.8,
       reviewCount: 2,
